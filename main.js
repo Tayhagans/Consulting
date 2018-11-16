e3e53115e2a748f0a3caadcef99354b399700489
tayhagans <tayhagans@gmail.com>
2018-11-15 23:24:44 -0500

    update

--- main.js
+++ main.js
@@ -7,25 +7,39 @@ $(document).ready(function() {
  
 });
 
-
-
  $(document).ready(function() {
+            $('.faq_question').click(function() {
+                if ($(this).parent().is('.open')) {
+                    $(this).closest('.faq').find('.faq_answer_container').slideUp();
+                    $(this).closest('.faq').removeClass('open');
+                } else {
+                    $('.faq_answer_container').slideUp();
+                    $('.faq').removeClass('open');
+                    $(this).closest('.faq').find('.faq_answer_container').slideDown();
+                    $(this).closest('.faq').addClass('open');
+                }
+            });
+        });
 
-  $('.faq_question').click(function() {
 
-    if ($(this).parent().is('.open')){
-      $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
-      $(this).closest('.faq').removeClass('open');
 
-      }else{
-        var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
-        $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);
-        $(this).closest('.faq').addClass('open');
-      }
+//  $(document).ready(function() {
 
-  });
+//   $('.faq_question').click(function() {
 
-});
+//     if ($(this).parent().is('.open')){
+//       $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
+//       $(this).closest('.faq').removeClass('open');
+
+//       }else{
+//         var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
+//         $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);
+//         $(this).closest('.faq').addClass('open');
+//       }
+
+//   });
+
+// });
 
  window.onscroll = function() {scrollFunction()};
 
